3f85c99140000904ed59fe120ff89b15f34eecc6
Mingwei Tang <mingwt@uw.edu>
2016-12-01 22:47:52 -0800

    little

--- src/basic_util.h
+++ src/basic_util.h
@@ -8,12 +8,12 @@
 #include<math.h>
 using namespace Rcpp;
 using namespace arma;
+
 arma::mat inv2(arma::mat a);
 arma::mat chols(arma::mat S);
 arma::mat mvrnormArma(int n, arma::mat sigma);
 
 arma::mat expM(arma::mat A);
 
-//arma::mat ODE(arma::vec initial, arma::vec t, arma::vec param);
 
 #endif /* BASIC_UTIL_H_ */
